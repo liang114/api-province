29e1e8d8667ddb3bf6580e309acc935c7bc70435
supapong <supapong@deepscope.com>
2024-08-08 14:42:38 +0700

    update cors check and caching

--- src/index.js
+++ src/index.js
@@ -3,12 +3,33 @@ const app = express();
 const mockData = require("../src/mockProvince.json");
 const port = 3000;
 const cors = require("cors");
+const apicache = require("apicache");
+
+
+const cache = apicache.middleware;
+
+app.use(cache("10 minutes"));
+
+const allowedOrigins = [
+  "https://dev-wcf.soilfish.co/",
+  "http://localhost:3000",
+];
 
 const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  credentials: true, // Allow cookies to be sent
-  optionsSuccessStatus: 204, // Some legacy browsers (IE11, various SmartTVs) choke on 204
+  allowedHeaders: ["Content-Type", "Authorization"],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
 };
+
+
 
 app.use(cors(corsOptions));
 
