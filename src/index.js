384f623130b2356f16ab1cc8123bea66fbecc6db
Supapong Engkasuwansiri <supapong_eng@yahoo.com>
2024-08-09 14:14:21 +0700

    fix cors

--- src/index.js
+++ src/index.js
@@ -7,36 +7,24 @@ const apicache = require("apicache");
 
 
 const cache = apicache.middleware;
-
 app.use(cache("10 minutes"));
-
-const allowedOrigins = [
-  "https://dev-wcf.soilfish.co/",
-  "http://localhost:3000",
-];
+app.use(express.json());
 
 const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: "*",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   allowedHeaders: ["Content-Type", "Authorization"],
   preflightContinue: false,
   optionsSuccessStatus: 204,
 };
 
-
-
 app.use(cors(corsOptions));
 
-app.get("/api/v1/address", (req, res) => {
-    
-  res.send(mockData);
+
+app.get("/api/v1/address", (_req, res) => {
+  res.status(200).json(mockData);
 });
+
 
 app.listen(port, () => {
   console.log(`Server start listening on port ${port}`);
